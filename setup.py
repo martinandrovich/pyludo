d4e11e1daf7c072a4f4536e5fcb5cb156b02d261
Martin Androvich <martinandrovich@gmail.com>
2021-07-29 22:45:58 +0200

    update setup.py metadata

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ from setuptools import setup
 setup(
 	name='pyludo',
 	version='1.0.0',
+	description='A python LUDO game simulator. Forked from RasmusHaugaard.',
+	author='Martin Androvich',
+	url='https://github.com/martinandrovich/pyludo',
+	packages=['pyludo'],
 	install_requires=[
 		'numpy',
 		'pyglet',
