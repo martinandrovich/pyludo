68a814ffce2e807e743367d509ab610aa1cdebc3
Martin Androvich <martinandrovich@gmail.com>
2021-08-01 17:43:32 +0200

    update examples

---
+++ pyludo/examples/game_ql_visualize.py
@@ -0,0 +1,19 @@
+import pyglet
+
+from pyludo import LudoGame, LudoVisualizerStep
+from pyludo.players import LudoPlayerRandom
+from pyludo.player_ql import LudoPlayerQLearning
+
+
+players = [
+	LudoPlayerQLearning(),
+	LudoPlayerRandom(),
+	LudoPlayerRandom(),
+	LudoPlayerRandom(),
+]
+
+game = LudoGame(players, info=True)
+window = LudoVisualizerStep(game)
+
+print("Use LEFT and RIGHT arrow to progress game.")
+pyglet.app.run()
